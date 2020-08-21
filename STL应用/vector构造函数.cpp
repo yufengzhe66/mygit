1d041e90a94a34870a662c0b012f307b65f4ca49
yufengzhe66 <17808459+yufengzhe66@users.noreply.github.com>
2020-08-21 13:35:54 +0800

    Add files via upload

---
+++ STL应用/vector构造函数.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+/*
+vector<T> v; //采用模板实现类实现，默认构造函数
+vector(v.begin(), v.end()); //将v[begin(), end())区间中的元素拷贝给本身。
+vector(n, elem); //构造函数将n个elem拷贝给本身。
+vector(const vector &vec); //拷贝构造函数。
+*/
+
+void printVector(vector<int>& v)
+{
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << "  ";
+	}
+	cout << endl;
+}
+
+
+void test12()
+{
+	vector<int>v1;//无参构造
+
+	for (int i = 0; i < 10; i++)
+	{
+		v1.push_back(i);
+	}
+	vector<int>v2(v1.begin(),v1.end());
+	printVector(v2);
+
+	vector<int>v3(10, 100);
+	printVector(v3);
+
+	vector<int>v4(v3);
+	printVector(v4);
+}
+
+int main()
+{
+	test12();
+	return 0;
+}
