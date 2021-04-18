9824f2760f800f1a842f6bcb8fb0c06d6803097e
yufengzhe66 <17808459+yufengzhe66@users.noreply.github.com>
2021-04-18 19:06:06 +0800

    Add files via upload

---
+++ 数据结构与算法/qsort.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int paritition(vector<int>& arr, int low, int high)
+{
+    int value = arr[low];
+    while (low < high)
+    {
+        while (arr[high] > value && low < high)
+            high--;
+        arr[low] = arr[high];
+        while (arr[low] < value && low < high)
+            low++;
+        arr[high] = arr[low];
+    }
+    arr[low] = value;
+    return low;
+}
+
+void qsort(vector<int>& arr, int low, int high)
+{
+    if (low < high)
+    {
+        int par = paritition(arr, low, high);
+        qsort(arr, low, par - 1);
+        qsort(arr, par + 1, high);
+    }
+}
+int main()
+{
+    vector<int> arr = {4,5,6,9,8,7,2,1,3};
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    qsort(arr, 0, arr.size() - 1);
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
+
