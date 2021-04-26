a84a253917423def3a0b43878f8795fc06817626
yufengzhe66 <17808459+yufengzhe66@users.noreply.github.com>
2021-04-26 23:34:00 +0800

    Add files via upload

---
+++ 数据结构/并查集.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> par;
+
+vector<int> ran;
+
+//初始化n个元素
+void init(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		par.push_back(i);
+		ran.push_back(0);
+	}
+}
+
+
+//查询树的根
+int find(int x)
+{
+	if (par[x] == x)
+		return x;
+	else
+		return par[x] = find(par[x]);
+}
+
+//合并x和y所属的集合
+void unite(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+
+	if (x == y)
+		return;
+	if (ran[x] < ran[y])
+		par[x] = y;
+	else
+	{
+		par[y] = x;
+		if (ran[x] == ran[y])
+			ran[x]++;
+	}
+}
+
+//判断x和y是否属于同一集合
+bool same(int x, int y)
+{
+	return find(x) == find(y);
+}
+
+
+int main()
+{
+	return 0;
+}
